c10c049cd3a45a9f5044054efd27e86cd21e1ded
Donald McPartland <dmcpartland@healthequity.com>
2017-02-14 16:23:31 -0700

    created cheat sheet

--- www/js/app.js
+++ www/js/app.js
@@ -76,13 +76,6 @@ angular.module('starter', ['ionic', 'ngCordova'])
       });
     }
 
-    $scope.savePicture1 = function () {
-      var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
-
-
-      window.location.href = image; // it will save locally
-    };
-
     $scope.savePicture = function () {
       var filePath = "./something.jpg";
       var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
